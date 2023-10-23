10717844fb4fb4add512babc8e0a241a43f7a753
chaeyonieee <141098135+chaeyonieee@users.noreply.github.com>
2023-10-23 23:19:04 +0900

    Deeper into Go

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades"	//var: about to create a new cariable
+	//=
+	card := "Ace of spades" //초기화 변수일때만 := 사용하기
+	// card : name of the variable 
+  card = "Diamond 5"
+
+	fmt.Println(card)
+}
