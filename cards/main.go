8e08e94b85b55a2e249bcea9e43ea5bf67891d5e
chaeyonieee <141098135+chaeyonieee@users.noreply.github.com>
2023-10-26 14:51:42 +0900

    Go Approach

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"	//var: about to create a new cariable
-	//=
-	card := "Ace of spades" //초기화 변수일때만 := 사용하기
-	// card : name of the variable 
-  card = "Diamond 5"
-
-	fmt.Println(card)
+	
+	card :=[]string{"3 diamond", newCard()}
+	card = append(card, "7 diamond")
+for i , card :=range card{
+	fmt.Println(i,card)
 }
+
+}
+
+
+func newCard() string{
+	return "5 diamond"
+} 
